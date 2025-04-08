ba24a77950db1405af9b663391dbbfc18ccfafa3
krish <krishhmakharia@gmail.com>
2025-04-08 18:52:08 +0530

    rename with docs

--- docs/app.js
+++ docs/app.js
@@ -4,21 +4,28 @@ let msg_box=document.querySelector("#msg-container");
 let msg=document.querySelector("#msg");
 let new_game=document.querySelector("#new-btn");
 let turnO=false;
+let count=0;
 const winning_conditions=[[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]];
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
         // console.log("box clicked");
         // box.style.backgroundColor="Black";
+        count++;
         if(turnO){
-            box.innerText="O";
+            box.innerHTML="<i class='text-yellow-600 font-bold'>O</i>";
             turnO=false;
         }
         else{
-            box.innerText="X";
+            box.innerHTML="<i class='text-red-600 font-bold'>X</i>";
             turnO=true;
         }
+        
         box.classList.add("disabled");
         checkWinner();
+        if(count===9){
+            msg.innerText="It's a tie";
+            msg_box.classList.remove("hidden");
+        }
     });
 });
 const disableBoxes=()=>{
